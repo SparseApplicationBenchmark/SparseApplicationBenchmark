f2f712b385f34980804698538bfbc094bca8d3fe
Richard Wan <myrst131@gmail.com>
2026-01-12 23:39:51 -0500

    fastsv: updated benchmark function

--- src/sparseappbench/benchmarks/fastsv.py
+++ src/sparseappbench/benchmarks/fastsv.py
@@ -16,46 +16,47 @@ Parallel Processing for Scientific Computing (pp. 46-57). Society for Industrial
 Applied Mathematics.
 
 Statement on the use of Generative AI: No generative AI was used to construct
-the benchmark function itself. Generative AI might have been used to construct tests.
-This statement was written by hand.
+the benchmark function itself. Generative AI was used for debugging. Generative
+AI might have been used to construct tests. This statement was written by hand.
 """
 
 
 def benchmark_fastsv(xp, adjacency_matrix):
     edges = xp.from_benchmark(adjacency_matrix)
-    edges = xp.lazy(edges)
 
     (n, m) = edges.shape
     assert n == m
 
     f = xp.arange(n)
+    gf = xp.asarray(f, copy=True)
 
-    # for edge traversal
-    rows, cols = xp.nonzero(edges)
-
-    f, rows, cols = xp.compute([f, rows, cols])
     while True:
-        f = xp.lazy(f)
+        dup = gf
 
-        f_next = xp.copy(f)
+        edges, f, gf = xp.lazy([edges, f, gf])
 
-        # step 1 (stochastic hooking)
-        mask = f_next[f[rows]] > f[f[cols]]
-        f_next[f[rows][mask]] = f[f[cols]][mask]
+        # step 1: stochastic hooking
+        gf_row = xp.expand_dims(gf, 0)
+        neighbor_gp = xp.where(edges != 0, gf_row, n)
+        mngf = xp.min(neighbor_gp, axis=1)
 
-        # step 2 (aggressive hooking)
-        mask = f_next[rows] > f[f[cols]]
-        f_next[rows[mask]] = f[f[cols]][mask]
+        # step 2: aggressive hooking
+        f = xp.minimum(f, mngf)
 
-        # step 3 (shortcutting)
-        f_next = xp.minimum(f_next, f[f])
+        # step 3: shortcutting
+        f = xp.minimum(f, gf)
 
-        f_prev = f
-        f = f_next
+        # step 4: calculate grandparents
+        gf = xp.take(f, f)
 
-        f, f_prev = xp.compute([f, f_prev])
+        dup, gf = xp.compute([dup, gf])
 
-        if xp.all(f[f] == f_prev[f_prev]):
+        # step 5: check termination
+        stop = xp.all(dup == gf)
+
+        f, gf, stop = xp.compute([f, gf, stop])
+
+        if stop:
             break
 
     return xp.to_benchmark(f)
