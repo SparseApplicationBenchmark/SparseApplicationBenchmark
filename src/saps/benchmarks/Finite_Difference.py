7da49b0fdcc005464fda878f161cf223347de7bf
Willow Ahrens <willow.marie.ahrens@gmail.com>
2026-09-08 00:59:02 -0400

    idk

--- src/saps/benchmarks/Finite_Difference.py
+++ src/saps/benchmarks/Finite_Difference.py
@@ -179,7 +179,7 @@ class FiniteDifferenceGenerator(Generator[FiniteDifferenceDataset]):
             FiniteDifferenceDataset(
                 name="fd_realistic_scale",
                 pretty_name="Finite Difference Realistic Problem",
-                suites=[],
+                suites=["standard"],
                 Nx=250000,
                 dx=0.1,
                 Nt=1000,
